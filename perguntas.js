231abb18b8c6ac3f3a938d65f637313ac2295b88
raquelljesuss <00001106001357sp@al.educacao.sp.gov.br>
2024-11-07 10:00:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O Brasil é banhado por qual oceano?',
+    'Oceano Atlantico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual continente fica o Egito?',
+    'Continente Africano'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a língua oficial da Arabia Saudita?',
+    'Arábe'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior rio do Brasil ?',
+    'Rio Amazonas'
+)
